c717dbd9a998c7c3701ee9d5cf50a9d2a7088a4e
Chinmay Prusty <kanha12297@gmail.com>
2024-01-13 21:52:42 +0530

    How to create a task and start it.

--- TaskProgramming/TaskProgramming/Program.cs
+++ TaskProgramming/TaskProgramming/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+
+    public static void Write(char c)
+    {
+        int i = 1000;
+        while(i -- > 0)
+        {
+            Console.Write(c);
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+        Task.Factory.StartNew(() => Write('.'));  //creating a task and starting it simultaneously
+
+        var t = new Task(()=> Write('?'));        //creating a task variable which is yet to start
+
+        t.Start();
+
+        Write('-');
+
+        Console.Write("Main Program done");
+
+        Console.Write("Hello, World!");
+    }
+}
